964fe8db13942f72a9029301c741f1b8040065e3
yama <yamamoto@kyms.jp>
2013-06-11 14:45:11 +0900

    プロパティコードをアップデート

--- module.cfFormDB.php
+++ module.cfFormDB.php
@@ -3,7 +3,7 @@
  * @version 1.0
  *
  * [モジュール設定]
- * &viewFields=一覧画面で表示する項目;text;&defaultView=デフォルト画面;list;list,csv;list
+ * @properties &viewFields=一覧画面で表示する項目;text; &defaultView=デフォルト画面;list;list,csv;list &sel_csv_fields=CSV出力項目を選択;list;1,0;1
  */
 include_once MODX_BASE_PATH . 'assets/modules/cfFormDB/cfformdb.class.php';
 
